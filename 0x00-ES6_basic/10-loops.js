7ed7149f250be50c374a83497cf69e230703183a
olayinka02 <oluwasegunyinka.samuel@gmail.com>
2023-09-28 19:50:45 +0000

    message

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,11 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-	
-    // eslint-disable-next-line
+  // eslint-disable-next-line
     for (const value of array) {
 
-      // eslint-disable-next-line
+    // eslint-disable-next-line
       array[array.indexOf(value)] = appendString + value;
-    }
-  
-    return array;
   }
+
+  return array;
+}
